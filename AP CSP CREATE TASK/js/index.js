37fc8d237512ed3609d323947885e7e06ea089f6
GhTmp1A <stanbutter4@gmail.com>
2023-02-21 11:56:34 -0500

    Progress on idea
    Need to fetch/acquire api objects in one function. Output the result into a div, then add 1 to an external counter. For next time function is run, if counter is 1, delete the existing div, and set counter back to 1.

--- AP CSP CREATE TASK/js/index.js
+++ AP CSP CREATE TASK/js/index.js
@@ -1,123 +1,55 @@
 import "../css/style.css";
-/*
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
-
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
- */
-
-/* import { data } from "./array";
-
-
-
-DOMSelectors.input.addEventListener("click", function () {
-  function chance(min, max) {
-    min = Math.ceil(1);
-    max = Math.floor(8);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  function gChance() {
-    let gumball = chance();
-    console.log(gumball);
-
-    data
-      .filter((gum) => gum.value == gumball)
-      .forEach((gumi) => console.log(gumi.flavor));
-    function gResult() {
-      data
-        .filter((gum) => gum.value == gumball)
-        .forEach((gumi) => {
-          console.log(gumi.rating);
-          if (gumi.rating >= 50) {
-            console.log("Acceptable");
-          } else {
-            console.log("no");
-          }
-        });
-    }
-    gResult();
-  }
-  gChance();
-});
-
-function chance(min, max) {
-  min = Math.ceil(1);
-  max = Math.floor(8);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function test() {
-  let gumball = chance();
-  console.log(gumball);
-
-  function test2() {
-    data
-      .filter((gum) => gum.value == gumball)
-      .forEach((gumi) => console.log(gumi.flavor));
-  }
-
-  function test3() {
-    console.log("Flavor:");
-    test2();
-    data
-      .filter((gum) => gum.value == gumball)
-      .forEach(
-        (gumi) =>
-          function () {
-            if (gumi.value >= 50) {
-              console.log("It is acceptable");
-            } else {
-              console.log("Unacceptable");
-            }
-          }
-      );
-  }
-
-  test3();
-}
-
-test();
- */
 
 const DOMSelectors = {
   display: document.getElementById("display"),
+  qbutton: document.getElementById("quote"),
 };
 
-const quotes = "https://api.goprogram.ai/inspiration";
-const dog = "https://random.dog/woof";
+const quotes = "https://api.quotable.io/random";
+const dog = "https://random.dog/woof.json";
 
-async function getQuote(quotes) {
+async function get(quotes) {
   try {
     const response = await fetch(quotes);
     const data = await response.json();
-    document.getElementById("api-response").innerHTML = data.quote;
+    /*     document.getElementById("api-response").innerHTML = data.content; */
+    DOMSelectors.display.insertAdjacentHTML(
+      "afterbegin",
+      `
+<div id = "test">
+<h1 id= "quoteHeader">${data.content}</h1>
+</div>
+`
+    );
     console.log(data);
   } catch (error) {
     console.log(error);
   }
 }
-getQuote(quotes);
 
-async function getDog() {
+/* async function getDog(dog) {
+  try {
+    const response = await fetch(dog);
+    const data = await response.json();
+    DOMSelectors.display.insertAdjacentHTML(
+      "afterbegin",
+
+      `
+      <div id = "dogDiv">
+      <img id = "dogImg" src = "${data.url}">
+      </div>
+      `
+    );
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+ */
+DOMSelectors.qbutton.addEventListener("click", function () {
+  get(quotes, dog);
+});
+/* async function getDog() {
   try {
     const response = await fetch(dog);
     if (response.status < 200 || response.status > 299) {
@@ -135,4 +67,4 @@ async function getDog() {
     console.log("Get outta here fix your code");
   }
 }
-getDog();
+getDog(); */
