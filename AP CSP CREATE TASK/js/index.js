3e9987247603cc110de51498803ded5692df8a92
GhTmp1A <stanbutter4@gmail.com>
2023-03-05 19:03:47 -0500

    Project done.

--- AP CSP CREATE TASK/js/index.js
+++ AP CSP CREATE TASK/js/index.js
@@ -6,123 +6,133 @@ const DOMSelectors = {
   reject: document.getElementById("reject"),
   accept: document.getElementById("accept"),
   apiSwitch: document.getElementById("imageSwitch"),
-  counter: document.getElementById("counter"),
   historyDisplay: document.getElementById("historydisplay"),
 };
-
-DOMSelectors.apiSwitch.addEventListener("click", function () {
-  if (DOMSelectors.display.classList.contains("dog")) {
-    DOMSelectors.display.classList.add("cat");
-    DOMSelectors.display.classList.remove("dog");
-    DOMSelectors.apiSwitch.innerHTML = `cats`;
-  } else {
-    DOMSelectors.display.classList.add("dog");
-    DOMSelectors.display.classList.remove("cat");
-    DOMSelectors.apiSwitch.innerHTML = `dogs`;
-  }
-}); // This function uses a button and click input by the user to change the class of the div in the html file where the output/result will be displayed. It detects whether or not this class is cat or dog, and if it is either cat or dog, then it switches it to the opposite name. Either way, the first result seen by the user will be an image of a cat, since everything is set up to be with a cat when the user first opens the web page. The user can then choose to run the code contuining to use the cat images exclusively, or to switch it to dog images, or back again.
-
 const history = [];
 const quotes = "https://api.quotable.io/random"; //quotes provided by the api.quoatable.io api. Github page: https://github.com/lukePeavey/quotable
 const dog = "https://random.dog/woof.json"; //images provided by the random.dog api.
-const cat = "https://cataas.com/cat?json=true"; //Images provided by the cats as a service rest api.
+const cat = "https://shibe.online/api/cats?count=1&urls=true&httpsUrls=false"; // images provided by the shibe.online api.
 
 async function get(quotes, dog, cat) {
   try {
     DOMSelectors.display.innerHTML = "";
     const response = await fetch(quotes);
     const response1 = await fetch(dog);
-    const response2 = await fetch(cat);
+    const response3 = await fetch(cat);
     const data = await response.json(history);
     const data1 = await response1.json(history);
-    const data2 = await response2.json(history); //functions fetches and retrieves all apis and returns usable json objects.
-    /*     document.getElementById("api-response").innerHTML = data.content; */
-
-    if (DOMSelectors.display.classList.contains("cat")) {
-      DOMSelectors.display.insertAdjacentHTML(
-        "afterbegin",
-        `
-  <div id = "output">
-  <h1 id= "quoteHeader">${data.content}</h1>
-  <img id = "catImg" src = https://cataas.com/${data2.url}>
-  </div>
-  `
-      ); //This code checks whether or not the div in where the results will be displayed contains the "cat" class. If it does, then the function will insert quote from the quote api regardless, however inserts a cat image and not a dog image.
-
-      history.push({ data, data2 });
-      console.log(history);
-      return history;
-    } else {
-      DOMSelectors.display.insertAdjacentHTML(
-        "afterbegin",
-        `
-      <div id = "output">
-      <h1 id= "quoteHeader">${data.content}</h1>
-      <img id = "dogImg" src = ${data1.url}>
-      </div>`
-      ); //This code checks whether or not the div in where the results will be displayed contains anything but the "cat" class. If it does contain a different class other than "cat" (in this case dog, as predetermined by the apiSwitch button), then the function will insert a quote from the quote api regardless of this class change, however inserts a dog image and not a cat image.
-    }
+    const data2 = await response3.json(history);
 
     console.log(data, data1, data2);
+    history.forEach((element) => history.splice(history.indexOf(element)));
+    history.push({ data, data1, data2 });
+    console.log(history);
+
+    function pageLoad() {
+      console.log(history);
+      if (DOMSelectors.display.classList.contains("cat")) {
+        history.forEach((item) =>
+          DOMSelectors.display.insertAdjacentHTML(
+            "afterbegin",
+            `  <div class = "output">
+        <h1 id= "quoteHeader">${item.data.content}</h1>
+  <img id = "catImg" src = ${item.data2[0]}>
+  </div>`
+          )
+        );
+      } else if (DOMSelectors.display.classList.contains("dog")) {
+        console.log(`dog`);
+        history.forEach((item) =>
+          DOMSelectors.display.insertAdjacentHTML(
+            "afterbegin",
+            `  <h1 id= "quoteHeader">${item.data.content}</h1>
+    <img id = "dogImg" src = ${item.data1.url}>
+    </div>`
+          )
+        );
+      }
+    }
+
+    pageLoad();
+
+    return history;
   } catch (error) {
     console.log(error);
   }
 }
 
-get(quotes, dog, cat); //This code will immediatly run upon the user loading the page. Because everything is set to "cat" by default, the user will automatically recieve an image of a cat.
+get(quotes, dog, cat);
 
-let i = 0; //Declares the used counter variable in global scope.
+function test1() {
+  if (DOMSelectors.display.classList.contains("dog")) {
+    DOMSelectors.display.classList.add("cat");
+    DOMSelectors.display.classList.remove("dog");
+    DOMSelectors.apiSwitch.innerHTML = `cat`;
+    DOMSelectors.display.innerHTML = "";
+    console.log(history);
+    console.log(`cat`);
+    history.forEach((item) =>
+      DOMSelectors.display.insertAdjacentHTML(
+        "afterbegin",
+        `  
+        <div class = "output">
+        <h1 id= "quoteHeader">${item.data.content}</h1>
+<img id = "catImg" src = ${item.data2[0]}>
+
+</div>`
+      )
+    );
+  } else if (DOMSelectors.display.classList.contains("cat")) {
+    DOMSelectors.display.classList.add("dog");
+    DOMSelectors.display.classList.remove("cat");
+    DOMSelectors.apiSwitch.innerHTML = `dog`;
+    DOMSelectors.display.innerHTML = "";
+    console.log(history);
+    console.log(`dog`);
+    history.forEach((item) =>
+      DOMSelectors.display.insertAdjacentHTML(
+        "afterbegin",
+        `  <div class = "output">
+        <h1 id= "quoteHeader">${item.data.content}</h1>
+<img id = "dogImg" src = ${item.data1.url}>
+</div>`
+      )
+    );
+  }
+}
+
+DOMSelectors.apiSwitch.addEventListener("click", function () {
+  test1();
+});
+// This function uses a button and click input by the user to change the class of the div in the html file where the output/result will be displayed. It detects whether or not this class is cat or dog, and if it is either cat or dog, then it switches it to the opposite name. Either way, the first result seen by the user will be an image of a cat, since everything is set up to be with a cat when the user first opens the web page. The user can then choose to run the code contuining to use the cat images exclusively, or to switch it to dog images, or back again.
+
 DOMSelectors.accept.addEventListener("click", function () {
   get(quotes, dog, cat); //Gets the quote, with the option of dog or cat depending on the user's choice from the earlier apiSwitch button.
   console.log(history);
-  history.forEach((item) =>
-    DOMSelectors.historyDisplay.insertAdjacentHTML(
-      "afterbegin",
-      `  <h1 id= "quoteHeader">${item.data.content}</h1>
-  <img id = "catImg" src = https://cataas.com/${item.data2.url}>
+  if (DOMSelectors.display.classList.contains("cat")) {
+    history.forEach((item) =>
+      DOMSelectors.historyDisplay.insertAdjacentHTML(
+        "afterbegin",
+        `  <div class = "output">
+        <h1 id= "quoteHeader">${item.data.content}</h1>
+  <img id = "catImg" src = ${item.data2[0]}>
   </div>`
-    )
-  );
-  console.log(history);
-  i++; //Adds the number "1" to the counter.
-  console.log(i);
-  DOMSelectors.counter.innerHTML = "";
-  DOMSelectors.counter.insertAdjacentHTML(
-    "afterbegin",
-    `<h2 class = "counterSize ">${i}</h2>`
-  );
-  //Inserts the new number that the counter now has into the page.
-  /*   history.push({ quotes, dog, cat });
-  console.log(history); */
+      )
+    );
+  } else {
+    history.forEach((item) =>
+      DOMSelectors.historyDisplay.insertAdjacentHTML(
+        "afterbegin",
+        `  <h1 id= "quoteHeader">${item.data.content}</h1>
+  <img id = "dogImg" src = ${item.data1.url}>
+  </div>`
+      )
+    );
+  }
 
-  /*   history.forEach(item) => DOMSelectors.historyDisplay.insertAdjacentHTML(
-    "afterbegin",
-    `
-  <div id = "outputHistory">
-  <h1 id= "quoteHeader">${element.data.content}</h1>
-  <img id = "catImg" src = https://cataas.com/${element.data2.url}>
-  </div>
-  `
-  ); */
+  console.log(history);
 });
 
 DOMSelectors.reject.addEventListener("click", function () {
   get(quotes, dog, cat);
-  if (i <= 0) {
-    //Checks whether or not counter is below or equal to zero.
-    DOMSelectors.counter.innerHTML = "";
-    let i = 0; //If the counter is below/equal to zero, the counter sets itself to the "0" number.
-    DOMSelectors.counter.insertAdjacentHTML(
-      "afterbegin",
-      `<h2 class = "counterSize ">${i}</h2>`
-    );
-  } else {
-    i--; //If the counter is, in this case, above 0, then it will subtract the number "1" from the counter.
-    console.log(i);
-    DOMSelectors.counter.innerHTML = "";
-    DOMSelectors.counter.insertAdjacentHTML(
-      "afterbegin",
-      `<h2 class = "counterSize ">${i}</h2>`
-    );
-  }
 });
