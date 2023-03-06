00a1117682e1903b9b745a2a7475d0872db98c13
GhTmp1A <stanbutter4@gmail.com>
2023-03-05 22:58:54 -0500

    Added comments.

--- AP CSP CREATE TASK/js/index.js
+++ AP CSP CREATE TASK/js/index.js
@@ -8,7 +8,8 @@ const DOMSelectors = {
   apiSwitch: document.getElementById("imageSwitch"),
   historyDisplay: document.getElementById("historydisplay"),
 };
-const history = [];
+
+const history = []; //History array is declared.
 const quotes = "https://api.quotable.io/random"; //quotes provided by the api.quoatable.io api. Github page: https://github.com/lukePeavey/quotable
 const dog = "https://random.dog/woof.json"; //images provided by the random.dog api.
 const cat = "https://shibe.online/api/cats?count=1&urls=true&httpsUrls=false"; // images provided by the shibe.online api.
@@ -21,34 +22,36 @@ async function get(quotes, dog, cat) {
     const response3 = await fetch(cat);
     const data = await response.json(history);
     const data1 = await response1.json(history);
-    const data2 = await response3.json(history);
+    const data2 = await response3.json(history); //function retrieves responses from the api's and turns them into usable json objects.
 
     console.log(data, data1, data2);
     history.forEach((element) => history.splice(history.indexOf(element)));
-    history.push({ data, data1, data2 });
+    history.push({ data, data1, data2 }); //Previous data in the history array is removed, and new data recieved from the function is now pushed into the array.
     console.log(history);
 
     function pageLoad() {
       console.log(history);
       if (DOMSelectors.display.classList.contains("cat")) {
-        history.forEach((item) =>
-          DOMSelectors.display.insertAdjacentHTML(
-            "afterbegin",
-            `  <div class = "output">
+        history.forEach(
+          (item) =>
+            DOMSelectors.display.insertAdjacentHTML(
+              "afterbegin",
+              `  <div class = "output">
         <h1 id= "quoteHeader">${item.data.content}</h1>
   <img id = "catImg" src = ${item.data2[0]}>
   </div>`
-          )
+            ) //If the display div class contains "cat" inside of it, then a cat image and a quote will be displayed.
         );
       } else if (DOMSelectors.display.classList.contains("dog")) {
         console.log(`dog`);
-        history.forEach((item) =>
-          DOMSelectors.display.insertAdjacentHTML(
-            "afterbegin",
-            `  <h1 id= "quoteHeader">${item.data.content}</h1>
+        history.forEach(
+          (item) =>
+            DOMSelectors.display.insertAdjacentHTML(
+              "afterbegin",
+              `  <h1 id= "quoteHeader">${item.data.content}</h1>
     <img id = "dogImg" src = ${item.data1.url}>
     </div>`
-          )
+            ) //If the display div class contains "cat" inside of it, then a dog image and a quote will be displayed.
         );
       }
     }
@@ -61,14 +64,14 @@ async function get(quotes, dog, cat) {
   }
 }
 
-get(quotes, dog, cat);
+get(quotes, dog, cat); //THe function is run in the global scope.
 
 function test1() {
   if (DOMSelectors.display.classList.contains("dog")) {
     DOMSelectors.display.classList.add("cat");
     DOMSelectors.display.classList.remove("dog");
     DOMSelectors.apiSwitch.innerHTML = `cat`;
-    DOMSelectors.display.innerHTML = "";
+    DOMSelectors.display.innerHTML = ""; //When the function is ran, it will replace the class inside of the display div with either "cat" or "dog" depending on what is already there.
     console.log(history);
     console.log(`cat`);
     history.forEach((item) =>
@@ -104,10 +107,9 @@ function test1() {
 DOMSelectors.apiSwitch.addEventListener("click", function () {
   test1();
 });
-// This function uses a button and click input by the user to change the class of the div in the html file where the output/result will be displayed. It detects whether or not this class is cat or dog, and if it is either cat or dog, then it switches it to the opposite name. Either way, the first result seen by the user will be an image of a cat, since everything is set up to be with a cat when the user first opens the web page. The user can then choose to run the code contuining to use the cat images exclusively, or to switch it to dog images, or back again.
 
 DOMSelectors.accept.addEventListener("click", function () {
-  get(quotes, dog, cat); //Gets the quote, with the option of dog or cat depending on the user's choice from the earlier apiSwitch button.
+  get(quotes, dog, cat);
   console.log(history);
   if (DOMSelectors.display.classList.contains("cat")) {
     history.forEach((item) =>
@@ -118,7 +120,7 @@ DOMSelectors.accept.addEventListener("click", function () {
   <img id = "catImg" src = ${item.data2[0]}>
   </div>`
       )
-    );
+    ); //If the user chooses to accept the image combination, then the function uses the history array to display the proper combination of image and quote.
   } else {
     history.forEach((item) =>
       DOMSelectors.historyDisplay.insertAdjacentHTML(
@@ -135,4 +137,4 @@ DOMSelectors.accept.addEventListener("click", function () {
 
 DOMSelectors.reject.addEventListener("click", function () {
   get(quotes, dog, cat);
-});
+}); //If the user decides to reject the image combination, then the function will simply be called again, the previous images and quote will be removed from the history array, replaced by a new combination.
